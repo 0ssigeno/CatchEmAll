9e2482c41cf0c63cb35526bc943e8851fa7f4dd0
0ssigeno <simone.berni2@studio.unibo.it>
2020-02-18 17:21:17 +0100

    More threads, and starting to create a test environ

--- main.py
+++ main.py
@@ -5,26 +5,20 @@ from threading import Thread
 
 log.basicConfig(level=log.INFO, format='%(threadName)s %(message)s')
 
-funcs=[]
-with open("functions.py","r") as f:
+funcs = []
+with open("functions.py", "r") as f:
     for line in f.readlines():
         if line.startswith("def"):
             funcs.append(line.split("(")[0].split(" ")[1])
 
-
-#es.db.populate_db("./vpn/toTest")
-#es.db.add_column("spotify")
-#es.db.add_column("nordvpnProxy")
-#
-threads=[]
+threads = []
 for func in funcs:
     method_to_call = getattr(functions, func)
-    es = ExecuteSites(max_threads=4, max_req_same_proxy=3)
+    es = ExecuteSites(max_threads=1, max_req_same_proxy=3)
     es.db.add_column(func)
-    thread = Thread(target=es.test_site,args=(method_to_call,func,))
+    thread = Thread(target=es.test_site, args=(method_to_call, func,))
     thread.start()
     log.info("Starting function {}".format(func))
     threads.append(thread)
 for t in threads:
     t.join()
-    #es.test_site(method_to_call, func)
