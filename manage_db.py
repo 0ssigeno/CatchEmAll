e7ee0291e98f03ac61a886400ddd83d0498d928d
0ssigeno <simone.berni2@studio.unibo.it>
2020-02-21 17:49:28 +0100

    Centralized db now

--- manage_db.py
+++ manage_db.py
@@ -8,34 +8,50 @@ import mysql.connector as mariadb
 
 class ManageDb:
     def __init__(self):
-        with open(".config.ini", "r") as f:
-            self.maria_usr, self.maria_pwd = f.readline().split(":")
-        self.maria_db = "Leaks"
-        self.maria_table = "Leaks"
-        self.regexCred = re.compile(
+        self._config_file = ".config.ini"
+        with open(self._config_file, "r") as f:
+            self.maria_usr, self.maria_pwd, self.maria_host = f.readline().split(":")
+        self._maria_db = "Leaks"
+        self._maria_table = "Leaks"
+        self._regexCred = re.compile(
             "[a-zA-Z0-9\\._-]+@[a-zA-Z0-9\\.-]+\.[a-zA-Z]{2,6}[\\rn :\_\-]{1,10}[a-zA-Z0-9\_\-]+")
+        self._privileges = {}
         self.__init_db()
-        self.connection, self.cursor = self.__create_table()
+        self._connection, self._cursor = self.__connection()
+        if "CREATE" or "ALL PRIVILEGES" in self._privileges[self._maria_db + ".*"]:
+            self.__create_table()
+
+    def __connection(self):
+        mariadb_connection = mariadb.connect(host=self.maria_host, user=self.maria_usr, password=self.maria_pwd,
+                                             database=self._maria_db)
+        cursor = mariadb_connection.cursor()
+        return mariadb_connection, cursor
 
     def __init_db(self):
-        mariadb_connection = mariadb.connect(host="localhost", user=self.maria_usr, password=self.maria_pwd)
+        mariadb_connection = mariadb.connect(host=self.maria_host, user=self.maria_usr, password=self.maria_pwd)
         cursor = mariadb_connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS `{}`".format(self.maria_db))
-        log.info("Database created")
+        privileges = "show grants for {}".format(self.maria_usr)
+        cursor.execute(privileges)
+        res = cursor.fetchall()
+
+        # TODO FIX THIS SHIT
+        for elem in res:
+            db = elem[0].split("ON")[1].split("TO")[0].strip().replace("`", "")
+            grants = [grant.strip() for grant in elem[0].split(" ON ")[0].split("GRANT")[1].split(",")]
+            self._privileges[db] = grants
+        if "CREATE" or "ALL PRIVILEGES" in self._privileges["*.*"]:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS `{}`".format(self._maria_db))
+            log.info("Database checked")
         mariadb_connection.disconnect()
 
     def __create_table(self):
-        mariadb_connection = mariadb.connect(host="localhost", user=self.maria_usr, password=self.maria_pwd,
-                                             database=self.maria_db)
-        cursor = mariadb_connection.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS `{}` (
+        self._cursor.execute("""CREATE TABLE IF NOT EXISTS `{}` (
                        `email` VARCHAR(320) NOT NULL,
                        `password` VARCHAR(320) NOT NULL,
                        PRIMARY KEY (email,password)
-                       )""".format(self.maria_table))
-        log.info("Table Leaks created")
-        mariadb_connection.commit()
-        return mariadb_connection, cursor
+                       )""".format(self._maria_table))
+        log.info("Table {} checked".format(self._maria_table))
+        self._connection.commit()
 
     def populate_db(self, mypath):
 
@@ -44,49 +60,49 @@ class ManageDb:
             if isfile(file_path):
                 with open(file_path, "r") as f:
                     text = f.read()
-                    itr = self.regexCred.finditer(text)
+                    itr = self._regexCred.finditer(text)
                     for elem in itr:
                         elem = elem.group()
                         creds = (elem.split(":")[0], elem.split(":")[1])
                         log.info("Inserting {}".format(creds))
                         insert = "INSERT IGNORE INTO Leaks(email,password) VALUES (%s,%s) "
-                        self.cursor.execute(insert, creds)
-        self.connection.commit()
+                        self._cursor.execute(insert, creds)
+        self._connection.commit()
 
     def add_column(self, name):
-        self.cursor.execute("ALTER TABLE Leaks ADD COLUMN IF NOT EXISTS {} BOOLEAN".format(name))
-        self.connection.commit()
+        self._cursor.execute("ALTER TABLE Leaks ADD COLUMN IF NOT EXISTS {} BOOLEAN".format(name))
+        self._connection.commit()
 
     def update_result(self, usr, pwd, column, result):
         update = "UPDATE Leaks SET {} = {} WHERE email= '{}' AND password= '{}'".format(column, result, usr, pwd)
-        self.cursor.execute(update)
-        self.connection.commit()
+        self._cursor.execute(update)
+        self._connection.commit()
 
     def retrieve_value_user(self, email, pwd, column):
         select = "SELECT {} from Leaks where email = '{}' and password = '{}'".format(column, email, pwd)
-        self.cursor.execute(select)
-        user = self.cursor.fetchone()[0]
+        self._cursor.execute(select)
+        user = self._cursor.fetchone()[0]
         return user
 
     def retrieve_users(self, column, value):
         select = "SELECT email,password from Leaks where {} is {}".format(column, value)
-        self.cursor.execute(select)
-        users = self.cursor.fetchall()
+        self._cursor.execute(select)
+        users = self._cursor.fetchall()
         return users
 
     def retrieve_all(self):
         select = "SELECT email, password from Leaks "
-        self.cursor.execute(select)
-        users = self.cursor.fetchall()
+        self._cursor.execute(select)
+        users = self._cursor.fetchall()
         return users
 
     def retrieve_proxies_names(self):
         """
         It retrieve every column that has `Proxy` in his name, so please be coherent or change this shit
         """
-        select = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}'".format(self.maria_table)
-        self.cursor.execute(select)
-        lists = self.cursor.fetchall()
+        select = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}'".format(self._maria_table)
+        self._cursor.execute(select)
+        lists = self._cursor.fetchall()
         results = []
         for table in lists:
             for column in table:
