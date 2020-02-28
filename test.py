01a1702aac34ffffb75f4231a6ed056eefb4f257
Simone Berni <simone.berni2@studio.unibo.it>
2020-02-28 23:54:37 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from manage_requests import ManageRequests
 
 if __name__ == "__main__":
     log.basicConfig(level=log.INFO, format='%(threadName)s %(message)s')
-    creds = "aaron1998evans@gmail.com:aaron1998"
+    creds = "usr:pwd"
     usr = creds.split(":")[0]
     pwd = creds.split(":")[1]
     print(functions.pornhub(usr, pwd, ManageRequests()))
